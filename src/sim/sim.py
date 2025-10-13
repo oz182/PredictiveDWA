683939f1117e63ff7a05a18b4e2fa8e0d4f1a8bd
ozelbaz <oz.elbaz@gmail.com>
2025-10-13 09:43:20 +0300

    Moved the path a little up from the right wall (by just changing the starting point)

--- src/sim/sim.py
+++ src/sim/sim.py
@@ -29,7 +29,7 @@ class Simulation:
         }
         
         # Initialize agents
-        self.robot = Robot((0.5, corridor_width/1.15), 0.2, self.corridor_bounds, self.get_door_position())
+        self.robot = Robot((0.5, corridor_width/1.25), 0.2, self.corridor_bounds, self.get_door_position())
         self.robot.set_goal((self.corridor_length - 1.0, corridor_width/1.2))
         
         # Set door information for DWA
